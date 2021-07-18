a1cee624ea61f69e22f7e7472a13b1106ad58fbc
Justin Aiello <justinaiello@gmail.com>
2021-07-18 18:49:11 -0400

    improvement(user): add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+
+  validates :password, length: { minimum: 8 }
+
+  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
 end
