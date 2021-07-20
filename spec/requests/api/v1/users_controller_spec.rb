55f7cba059de487980beb2089f7bc7c0209aaa95
Justin Aiello <justinaiello@gmail.com>
2021-07-19 22:55:55 -0400

    feat(user): add more request specs

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -29,4 +29,61 @@ RSpec.describe Api::V1::UsersController, type: :request do
       end
     end
   end
+
+  describe 'creating a user' do
+    let(:signup_params) do
+      {
+        user: {
+          name: 'Miley Cyrus',
+          email: signup_email,
+          password: 'partyinth3usa'
+        }
+      }
+    end
+
+    context 'when there are invalid or missing params' do
+      let(:signup_email) { nil }
+
+      it 'does not allow a character to be created' do
+        post api_v1_sign_up_path, params: signup_params
+
+        expect(response).to have_http_status(422)
+        expect(JSON.parse(response.body)[0]).to eq('Email is invalid')
+      end
+    end
+
+    context 'when all params are valid' do
+      let(:signup_email) { 'just@beingmiley.com' }
+
+      it 'creates a user' do
+        post api_v1_sign_up_path, params: signup_params
+
+        expect(response).to have_http_status(201)
+        expect(JSON.parse(response.body)['user']['name']).to eq(signup_params[:user][:name])
+      end
+    end
+  end
+
+  describe 'showing a logged in users information' do
+    context 'when the user is not logged in' do
+      it 'does not response with any user information' do
+        get api_v1_users_me_path
+
+        expect(response).to have_http_status(401)
+        expect(JSON.parse(response.body)['message']).to eq('Please log in')
+      end
+    end
+
+    context 'when the user is logged in' do
+      let(:user) { create(:user) }
+      let(:token) { JWT.encode({ user_id: user.id }, ENV['APP_SECRET']) }
+      let(:headers) { { 'Authorization': "Bearer #{token}" } }
+
+      it 'returns the user\'s info' do
+        get api_v1_users_me_path, headers: headers
+
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
